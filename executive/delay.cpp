81483d071dac034a0c0b6ea0c944f6c7f40d0e07
software1 <1425sw@gmail.com>
2016-11-28 19:20:20 -0800

    Delete buttons from pervious year

--- executive/delay.cpp
+++ executive/delay.cpp
@@ -7,41 +7,29 @@ using namespace std;
 
 using Mode=Executive;
 
-Executive get_auto1(Next_mode_info /*info*/){
-	/*if (info.panel.in_use) {
-		switch(info.panel.auto_mode){ 
-			case Panel::Auto_mode::NOTHING:
+Executive auto_mode_convert(Next_mode_info info){
+	if (info.panel.in_use) {
+		switch(info.panel.auto_select){ 
+			case 1: 
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 0:
+			default:
 				return Executive{Auto_null()};
-			case Panel::Auto_mode::REACH:
-				return Executive{Auto_reach()};
-			case Panel::Auto_mode::STATICS:
-				return Executive{Auto_statictwo()};
-			case Panel::Auto_mode::STATICF:
-				return Executive{Auto_static()};
-			case Panel::Auto_mode::PORTCULLIS:
-				return Mode{Auto_portcullis()};
-			case Panel::Auto_mode::CHEVAL:
-				return Mode{Auto_cheval_pos()};
-			case Panel::Auto_mode::LBLS:
-				return Mode{Auto_lbls_cross_lb(info.status.drive.ticks)};
-			case Panel::Auto_mode::LBWLS:	
-				return Mode{Auto_lbwls_wall()};
-			case Panel::Auto_mode::LBWHS:
-				return Mode{Auto_lbwhs_wall()};
-			case Panel::Auto_mode::S:
-				return Mode{Auto_lbwhs_prep()};
-			case Panel::Auto_mode::BR:
-				//FIXME: For now, just choosing some number to put in.
-				return Mode{Auto_br_straightaway(0, info.status.drive.ticks)};
-			default: assert(0);
 		}
-	}*/
+	}
 	return Mode{Delay()};
 }
 
 Mode Delay::next_mode(Next_mode_info info){
 	if(!info.autonomous) return Mode{Teleop()};
-	if(info.since_switch > (info.panel.speed_dial+1)*5 || info.since_switch > 8) return get_auto1(info);
+	if(info.since_switch > 8) return auto_mode_convert(info);
 	return Mode{Delay()};
 }
 
